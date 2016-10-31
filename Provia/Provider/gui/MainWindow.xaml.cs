2c46581d8524fedf6113751d2ddb04b234f48da6
Niels <Niels@NIELSLAPTOP>
2016-10-31 17:20:19 +0100

    LORT VIRKER

--- Provia/Provider/gui/MainWindow.xaml.cs
+++ Provia/Provider/gui/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Provider.gui
+namespace Provider
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
