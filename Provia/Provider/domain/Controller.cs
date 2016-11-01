bf07cbb4324d63caeee502f757500c3b1f095356
Karim <Karim@KARIM-BPC>
2016-11-01 14:55:11 +0100

    Controller skelettet er implemteret.

--- Provia/Provider/domain/Controller.cs
+++ Provia/Provider/domain/Controller.cs
@@ -1,17 +1,41 @@
-﻿using System;
+﻿using Provider.domain.page;
+using Provider.domain.users;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.SystemException;
 
 namespace Provider.domain
 {
     class Controller
     {
+        private static Controller instance;
+
+        public Controller GetInstance()
+        {
+            if(instance == null)
+            {
+                instance = new Controller();                
+            }           
+                return instance;        
+        }
+        public List<Supplier> ViewSuppliers()
+        {
+            ///TODO: to be implementede
+            throw new NotImplementedException();
+        }
+
+        public Page GetSupplierPage(Supplier s)
+        {
+            ///TODO: to be implementede
+            throw new NotImplementedException();
+        }
+
         public Boolean Login(String Name, String Password)
         {
-            throw NotImplementedException();
+            ///TODO: to be implementede
+            throw new NotImplementedException();
         }
     }
 }
