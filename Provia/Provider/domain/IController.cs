96baa012646f32d88554506ed3c8e458b5b6617a
Joachim Hejlesen <johej15@student.sdu.dk>
2016-11-02 11:50:00 +0100

    Created ViewSuppliers in interface IController
    Why do I write in English?

--- Provia/Provider/domain/IController.cs
+++ Provia/Provider/domain/IController.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using Provider.domain.page;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Provider.domain
 {
-    class IController
+    public interface IController
     {
+
+         List<Page> ViewSuppliers();
     }
 }
