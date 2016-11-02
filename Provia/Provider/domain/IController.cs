100f021bd95a9daaa58f0b33f8ac5e72e1ee92f1
Joachim Hejlesen <johej15@student.sdu.dk>
2016-11-02 13:06:59 +0100

    Lavet Interface til Controller (Kaldet IController) Oprettet metode ViewSupplier i IController

---
+++ Provia/Provider/domain/IController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using Provider.domain.page;
+using Provider.domain.users;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Provider.domain
+{
+    public interface IController
+    {
+        List<Page> ViewSuppliers();
+    }
+}
