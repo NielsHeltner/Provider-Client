a99f8711510fd10758504d56e7e12482c1290e52
Jebi <Jebisan.h.n@hotmail.com>
2016-11-02 15:34:47 +0100

    C# doc. for Validate.

--- Provia/Provider/domain/users/UserManager.cs
+++ Provia/Provider/domain/users/UserManager.cs
@@ -18,6 +18,14 @@ namespace Provider.domain.users
             users.Add(new Provia("Jebisan", "123"));
         }
 
+        /// <summary>
+        /// Checks if the userName is found in the set of users, and then checks the users password. 
+        /// If user and password returns true, the user will be set as LoggedInUser. *NOT CASESENSITIVE*
+        /// </summary>
+        /// <param name="userName">The logging in users username</param>
+        /// <param name="password">The logging in users password</param>
+        /// <returns> True if username and password matches.  </returns>
+
         public bool Validate(string userName, string password)
         {
             foreach(AbstractUser user in users)
