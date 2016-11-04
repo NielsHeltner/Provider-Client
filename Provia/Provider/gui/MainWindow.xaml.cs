f6f2a7f71e1cd748a84f2f7548143fd28e85b7fe
Niclas Mølby <niclas@molby.net>
2016-11-04 10:31:32 +0100

    test

--- Provia/Provider/gui/MainWindow.xaml.cs
+++ Provia/Provider/gui/MainWindow.xaml.cs
@@ -30,6 +30,7 @@ namespace Provider.gui
         {  
             InitializeComponent();
             frame.NavigationUIVisibility = NavigationUIVisibility.Hidden;
+            frame.NavigationService.RemoveBackEntry();
             SetVisibilityToHidden();
             List<Button> buttons = new List<Button>();
             buttons.Add(showSuppliersButton);
