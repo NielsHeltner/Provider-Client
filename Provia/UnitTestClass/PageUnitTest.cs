b98c3925142e0ab4c0fd3359337d97aefcfe3bb2
Joachim Hejlesen <johej15@student.sdu.dk>
2016-11-07 14:42:22 +0100

    Ændret searchTest til at teste resultatet.

--- Provia/UnitTestClass/PageUnitTest.cs
+++ Provia/UnitTestClass/PageUnitTest.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Provider.domain.page;
+using Provider.domain.users;
 using System.Collections.Generic;
 
 namespace UnitTestClass.page
@@ -10,7 +11,7 @@ namespace UnitTestClass.page
     {
         [TestMethod]
         public void ViewSuppliersTest()
-        {
+        { 
             PageManager pm = new PageManager();
             Assert.AreEqual(typeof(List<Provider.domain.page.Page>), pm.pages.GetType());
         }
@@ -19,7 +20,13 @@ namespace UnitTestClass.page
         public void SearchTest() // TODO: skal muligvis laves om, så den tester resultatet i stedet for typen
         {
             PageManager pm = new PageManager();
-            Assert.AreEqual(typeof(List<Provider.domain.page.Page>), pm.Search("e").GetType());
+            pm.pages.Add(new Page(new Supplier("DSM Nutritional Products Ltd", "somepswd")));
+            pm.pages.Add(new Page(new Supplier("Fenchem Biotek Ltd", "somepswd")));
+            pm.pages.Add(new Page(new Supplier("Chr.Olesen Nutrition A/S", "somepswd")));
+            Assert.AreEqual("Chr.Olesen Nutrition A/S",pm.Search("chr")[0].name);
+            Assert.AreEqual("DSM Nutritional Products Ltd", pm.Search("DSM")[0].name);
+            Assert.AreEqual(2, pm.Search("Ltd").Count);
+            //Assert.AreEqual(typeof(List<Provider.domain.page.Page>), pm.Search("e").GetType());
         }
 
     }
