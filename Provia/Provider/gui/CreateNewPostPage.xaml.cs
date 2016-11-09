ecb002aa5b1a9d3ef5e483017d0c3f1433b18262
Karim <Karim@KARIM-BPC>
2016-11-09 19:19:30 +0100

    rettelser på opret post

--- Provia/Provider/gui/CreateNewPostPage.xaml.cs
+++ Provia/Provider/gui/CreateNewPostPage.xaml.cs
@@ -36,15 +36,23 @@ namespace Provider.gui
         /// skal måske laves om... det snakker vi lige om
         private void CreateNewPost(object sender, RoutedEventArgs e)
         {
-            if(!string.IsNullOrWhiteSpace(PostDescriptionTextBox.Text) && !string.IsNullOrWhiteSpace(TitleTextBox.Text))
+            if (!string.IsNullOrWhiteSpace(PostDescriptionTextBox.Text) && !string.IsNullOrWhiteSpace(postTitleTextBox.Text))
             {
-                CreatePost();
-            }
-            else
+                if (WarningRB.IsChecked == false && OfferRB.IsChecked == false && requestRB.IsChecked == false)
+                {
+                    SomthingWentWrongLabel.Content = "Husk at vælge katagori";
+                    SomthingWentWrongLabel.Visibility = Visibility.Visible;
+                } else
+                {
+                    CreatePost();
+                } 
+
+            } else
             {
+                SomthingWentWrongLabel.Content = "Der skal være noget indhold dit opslag";
                 SomthingWentWrongLabel.Visibility = Visibility.Visible;
             }
-               
+              
         }
         private void CreatePost()
         {
@@ -66,10 +74,30 @@ namespace Provider.gui
             {
                 typeOfPost = domain.bulletinboard.Post.Types.NotAvailabe;
             }
-                Controller.instance.CreatePost(Controller.instance.GetLoggedInUserName(), TitleTextBox.Text, PostDescriptionTextBox.Text, typeOfPost);
+                Controller.instance.CreatePost(Controller.instance.GetLoggedInUserName(), postTitleTextBox.Text, PostDescriptionTextBox.Text, typeOfPost);
                 bulletinBoardPage.RefreshPage();
         }
-        
-        
+
+        private void TitleLostFocus(object sender, RoutedEventArgs e)
+        {
+            if (postTitleTextBox.Text.Length == 0)
+                titleText.Visibility = Visibility.Visible;
+        }
+
+        private void TitleGotFocus(object sender, RoutedEventArgs e)
+        {
+            titleText.Visibility = Visibility.Hidden;
+        }
+
+        private void PostDescriptionLostFocus(object sender, RoutedEventArgs e)
+        {
+            if (PostDescriptionTextBox.Text.Length == 0)
+                desriptionText.Visibility = Visibility.Visible;
+        }
+
+        private void PostDescriptionGotFocus(object sender, RoutedEventArgs e)
+        {
+            desriptionText.Visibility = Visibility.Hidden;
+        }
     }
 }
