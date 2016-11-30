ca1aa2b39409b5d8da3f7fde24a98f40208c6381
NiclasMolby <niclas@molby.net>
2016-11-30 14:09:45 +0100

    Kan ikke huske hvad har jeg lavet her, men det er en reetelse

--- Provider/gui/SupplierGroupBox.xaml.cs
+++ Provider/gui/SupplierGroupBox.xaml.cs
@@ -10,17 +10,20 @@ namespace Provider.gui
     /// </summary>
     public partial class SupplierGroupBox : Page
     {
-        private IO.Swagger.Model.Page page2;
+        private IO.Swagger.Model.Page Page;
 
-        public SupplierGroupBox(IO.Swagger.Model.Page page)
+        public SupplierGroupBox(IO.Swagger.Model.Page Page)
         {
             InitializeComponent();
-            this.page2 = page;
-            if(page2.Note != null)
+            this.Page = Page;
+            ContactInformation.Text = Page.ContactInformation;
+            Location.Text = Page.Location;
+            Description.Text = Page.Description;
+            if(this.Page.Note != null)
             {
-                noteTextBox.Text = page2.Note.Text;
-                lastEdited.Text = ((DateTime) page2.Note.CreationDate).ToLongDateString() + ".";
-                lastEditor.Text = page2.Note.Editor;
+                noteTextBox.Text = this.Page.Note.Text;
+                lastEdited.Text = ((DateTime) this.Page.Note.CreationDate).ToLongDateString() + ".";
+                lastEditor.Text = this.Page.Note.Editor;
             }
         }
 
@@ -37,7 +40,7 @@ namespace Provider.gui
                 editNote.Content = "Rediger notater";
                 lastEdited.Text = DateTime.Today.ToLongDateString() + ".";
                 lastEditor.Text = Controller.instance.GetLoggedInUser().Username;
-                Controller.instance.AddNoteToSupplier(page2.Owner, Controller.instance.GetLoggedInUser().Username, noteTextBox.Text);
+                Controller.instance.AddNoteToSupplier(Page.Owner, Controller.instance.GetLoggedInUser().Username, noteTextBox.Text);
             }
         }
     }
