66eeb62a42cb06b4af0788c2a1d4d8033fb0976b
Jebi <Jebisan.h.n@hotmail.com>
2016-12-05 16:52:11 +0100

    Pisset virker ikke

---
+++ Provider/gui/CreateNewProductPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows;
+using IO.Swagger.Model;
+using Provider.domain;
+using Provider.domain.bulletinboard;
+using Page = System.Windows.Controls.Page;
+
+namespace Provider.gui
+{
+    /// <summary>
+    /// Interaction logic for CreateNewPost.xaml
+    /// </summary>
+    public partial class CreateNewProductPage : Page
+    {
+     
+        public CreateNewProductPage()
+        {
+            InitializeComponent();
+        
+        }
+
+      
+    }
+}
